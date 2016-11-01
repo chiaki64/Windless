d3ab4139854254aed21df336ab8143a6a95cc6e1
Hieda no Chiaki <i@wind.moe>
2016-11-01 19:30:23 +0800

    remove unwanted files

--- core/components/mail.py
+++ core/components/mail.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import smtplib
-from email.header import Header
-from email.mime.text import MIMEText
-from getpass import getpass
-from utils import load_config
+from utils.shortcuts import load_config
 
 config = load_config()
 dev = config.get('dev')
@@ -15,17 +11,7 @@ class Mail:
     def __init__(self):
         pass
 
-    def server(self, host=config['mail']['host'], port=config['mail']['port']):
-        server = smtplib.SMTP_SSL(host, port)
-        server.login(config['mail']['account'], config['mail']['password'])
 
-    @property
-    def message(self, _from, _to, subject, content):
-        msg = MIMEText(content, 'plain', 'utf-8')
-        msg['From'] = _from
-        msg['To'] = _to
-        msg['Subject'] = Header(subject, 'utf-8')
-        return msg
 
 
 
