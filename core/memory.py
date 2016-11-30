db41e7fc31580dc14318a706ccabd85a09675bbd
Hieda no Chiaki <i@wind.moe>
2016-11-30 09:20:02 +0800

    Add annotation in memory.py

--- core/memory.py
+++ core/memory.py
@@ -14,8 +14,17 @@ class RedisFilter:
     def __init__(self, redis):
         self._connection = redis
 
-    # Key Control
     async def set(self, table, data, many=True, id=None):
+        """
+        封装 Set 操作，视传入数据的类型以不同形式存入 Redis
+        Args:
+            table:  表名
+            data:   数据
+            many:   数据是否多处存储
+            id:     数据存储的ID
+        Returns:
+            *
+        """
         key = self.prefix(table) + (
             '.' + ((str((await self.last(table)) + 1)) if id is None else str(id)) if many else '')
 
@@ -34,6 +43,15 @@ class RedisFilter:
         return (data['id'] if 'id' in data else data) if many else True
 
     async def lpush(self, table, value, isdict=False):
+        """
+        将数据存入以 List 格式存储的数据
+        Args:
+            table:  表名
+            value:  数据
+            isdict: 传入的数据是否是字典
+        Returns:
+            None
+        """
         key = self.prefix(table)
         data = await self.lget(table, isdict)
         if data is None:
@@ -43,6 +61,16 @@ class RedisFilter:
         await self._connection.set(key, json.dumps({'list': data}))
 
     async def lset(self, table, id, value, isdict=False):
+        """
+        修改以 List 格式存储的数据内容,如不存在则无改动保存
+        Args:
+            table:  表名
+            id:     要修改数据的id
+            value： 修改后的值
+            isdict: 修改的数据是否是字典
+        Returns:
+            None
+        """
         key = self.prefix(table)
         data = await self.lget(table, isdict)
         if data is None:
@@ -56,6 +84,14 @@ class RedisFilter:
         await self._connection.set(key, json.dumps({'list': data}))
 
     async def get(self, table, id=None):
+        """
+        封装 Get 操作，返回 json 数据
+        Args:
+            table:  表名
+            id:     获取数据的id
+        Returns:
+            json or str
+        """
         key = self.prefix(table) + ('.' + str(id) if id is not None else '')
         data = await self._connection.get(key)
         if data is None:
@@ -67,6 +103,15 @@ class RedisFilter:
         return data
 
     async def lget(self, table, isdict=False, reverse=True):
+        """
+        将数据从以 List 格式存储的数据中取出
+        Args:
+            table:   表名
+            isdict:  取出的数据是否为字典
+            reverse：取出的数据是否逆排列
+        Returns:
+            list
+        """
         data = await self.get(table)
         if data is not None:
             li = data['list']
@@ -83,12 +128,30 @@ class RedisFilter:
         return []
 
     async def delete(self, table, id=None):
+        """
+        封装 Delete 操作，实际上并没有什么用（
+        Args:
+            table:  表名
+            id:     需删除数据的id
+        Returns:
+            True
+        """
         key = self.prefix(table) + ('.' + str(id) if id is not None else '')
         await self._connection.delete(key)
         return True
 
     async def ldelete(self, table, value=None, isdict=False, _key='id'):
         # value为要删除的值，为空则全部删除
+        """
+        将数据从以 List 格式存储的数据中删除
+        Args:
+            table:  表名
+            value:  在列表中删除对应值的数据,为空则全部删除
+            isdict: 传入的数据格式是否为字典
+            _key:   如果要删除的数据是字典则根据_key的名称来删除对应数据
+        Returns:
+            True
+        """
         data = await self.lget(table, isdict)
         if value is not None:
             if isdict is False:
@@ -107,6 +170,15 @@ class RedisFilter:
         return True
 
     async def get_list(self, table, keys=None, isauth=False):
+        """
+        取出以 List 格式存储的数据
+        Args:
+            table:  表名
+            keys:   获取的数据的 id,以 list 形式传入
+            isauth: 访问者是否通过认证，返回某些仅所有者可查看的数据
+        Returns:
+            list
+        """
         data_list = []
         if keys is None:
             arr = await self._connection.keys(self.prefix(table) + '.*')
@@ -127,10 +199,16 @@ class RedisFilter:
         return data_list
 
     async def count(self, table):
+        """
+        返回当前表有多少条数据
+        """
         keys = await self._connection.keys(self.prefix(table) + '.*')
         return str(keys.__len__())
 
     async def last(self, table):
+        """
+        计算最后一篇文章的id,用以创建新文章时自增,即计算 Windless:Articles.* 类型的最后id
+        """
         id = 0
         keys = await self._connection.keys(self.prefix(table) + '.*')
         for item in keys:
@@ -141,30 +219,23 @@ class RedisFilter:
 
     @staticmethod
     def prefix(table):
+        """
+        给数据添加数据库名称 如: Windless:Archive
+        """
         return config['memory']['database'] + ':' + table
 
-    # Other
     async def close(self):
+        """
+        关闭数据库连接，暂时无用
+        :return:
+        """
         self._connection.close()
 
 
 async def fun():
     redis = await aioredis.create_redis(('localhost', 6379), loop=loop)
     redis = RedisFilter(redis)
-
-    # await redis.lpush('Test', {'id': '1'}, isdict=True)
-    # await redis.lpush('Test', {'id': '5'}, isdict=True)
-    # await redis.lpush('Test', {'id': '3'}, isdict=True)
-    # await redis.lpush('Test', {'id': '15'}, isdict=True)
-
-    print(await redis.lget('Test', isdict=True))
-
-    # await redis.lset('Test', '16', {'id': '17'},isdict=True)
-    await redis.ldelete('Test', isdict=True)
-    print(await redis.lget('Test', isdict=True))
-
-    print('finish')
-
+    # Test COde
     await redis.close()
 
 
