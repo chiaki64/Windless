7828131d4d5b4440d839cb4499929e4ed49f75a5
Hieda no Chiaki <i@wind.moe>
2016-12-25 15:03:56 +0800

    Add description meta in every article

--- core/views.py
+++ core/views.py
@@ -287,7 +287,7 @@ class BackendArticleUpdateView(AbsWebView):
         data['updated_time'] = int(str(time.time()).split('.')[0])
         # 分割文章
         data['desc'] = (data['html'])[:(data['html']).find('<hr>', 1)]
-        data['desc_text'] = ((data['text'])[:(data['text']).find('-----', 1)]).replace('\n', '')
+        data['desc_text'] = ((data['text'])[:(data['text']).find('-----', 1)]).replace('\n', '').replace('\"', '\'')
         # 删除分割线
         data['html'] = data['html'].replace('<hr>', '', 1)
 
