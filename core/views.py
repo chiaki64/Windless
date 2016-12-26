2922f0eed35d08ef6147e2a873786709a6b900dd
Hieda no Chiaki <i@wind.moe>
2016-12-27 00:49:09 +0800

    test u2f

--- core/views.py
+++ core/views.py
@@ -565,6 +565,5 @@ class APIHandler:
 
         challenge = user.pop('_u2f_challenge_')
         c, t = verify_authenticate(devices, challenge, data, [facet])
-        return web.json_response({
-            'touch': t
-        })
+        print(c, t)
+        return web.json_response(json.dumps(True))
