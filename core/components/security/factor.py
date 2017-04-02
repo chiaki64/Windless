07f7421a6cb6cb70c7c30bdbfc05258fe93a97ea
Hieda no Chiaki <i@wind.moe>
2017-04-02 16:32:37 +0800

    Clean u2d code

--- core/components/security/factor.py
+++ core/components/security/factor.py
@@ -8,7 +8,6 @@ from u2flib_server.u2f import (begin_registration,
                                complete_authentication)
 from components.eternity import config
 
-
 facet = config.rss['link']
 
 
@@ -17,21 +16,25 @@ async def enroll(user):
     user['_u2f_enroll_'] = enroll.json
     return user, json.dumps(enroll.data_for_client)
 
+
 async def bind(user, data):
     response = data['tokenResponse']
     enroll = user.pop('_u2f_enroll_')
-    device, cert = complete_registration(enroll, response, [facet])
-    patch = device
-    patch['deviceName'] = data['deviceName']
-    patch['registerDate'] = data['date']
-    user.setdefault('_u2f_devices_', []).append(json.dumps(patch))
-    # cert = x509.load_der_x509_certificate(cert, default_backend())
+    try:
+        device, cert = complete_registration(enroll, response, [facet])
+        device['deviceName'] = data['deviceName']
+        device['registerDate'] = data['date']
+        user.setdefault('_u2f_devices_', []).append(json.dumps(device))
+    except AttributeError:
+        return user, False
     return user, True
+
 
 async def sign(user):
     challenge = begin_authentication(facet, user.get('_u2f_devices_', []))
     user['_u2f_challenge_'] = challenge.json
     return user, json.dumps(challenge.data_for_client)
+
 
 async def verify(user, data):
     challenge = user.pop('_u2f_challenge_')
